5d3167bafa0b860ed2378a3d60e58ac25d45903b
arnaucube <arnaucube@gmail.com>
2019-02-13 20:45:21 +0100

    token transactions signed from wallet working

--- endpoint/hanlders.go
+++ endpoint/hanlders.go
@@ -1,6 +1,7 @@
 package endpoint
 
 import (
+	"encoding/hex"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -46,21 +47,30 @@ func handleGetTxNonce(c *gin.Context) {
 }
 
 type TxMsg struct {
-	From  common.Address
-	To    common.Address
-	Value *big.Int
-	R     [32]byte
-	S     [32]byte
-	V     byte
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value int    `json:"value"`
+	R     string `json:"r"`
+	S     string `json:"s"`
+	V     int    `json:"v"`
 }
 
 func handlePostTx(c *gin.Context) {
 	var tx TxMsg
 	c.BindJSON(&tx)
 
+	rBytes, err := hex.DecodeString(tx.R)
+	check(c, err)
+	var r32 [32]byte
+	copy(r32[:], rBytes)
+	sBytes, err := hex.DecodeString(tx.S)
+	check(c, err)
+	var s32 [32]byte
+	copy(s32[:], sBytes)
+
 	auth, err := eth.GetAuth()
 	check(c, err)
-	ethTx, err := ethSrv.Token.Transfer(auth, tx.From, tx.To, tx.Value, tx.R, tx.S, tx.V)
+	ethTx, err := ethSrv.Token.Transfer(auth, common.HexToAddress(tx.From), common.HexToAddress(tx.To), big.NewInt(int64(tx.Value)), r32, s32, byte(tx.V))
 	check(c, err)
 	c.JSON(200, gin.H{
 		"ethTx": ethTx.Hash().Hex(),
